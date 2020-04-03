8948b3a63500b3904b98af0b125c3725af3f7616
Andrii Krupskyi <andrey.krupskii@sisense.com>
2020-04-03 13:10:37 +0300

    Fixed routes.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const Store = require('./Store');
 
+process.on('uncaughtException', (e) => {
+  console.error('Occurs uncaught exception:', e);
+});
+process.on('unhandledRejection', (e) => {
+  console.error('Occurs unhandled rejection:', e);
+});
+
 const SECRET = process.env.SECRET;
 const PORT = process.env.PORT || 3000;
 
@@ -12,10 +19,12 @@ const store = new Store();
 app.use(bodyParser.json());
 
 app.use('/', (req, res, next) => {
-  console.log(`Process incoming request: ${req.originalUrl}`);
+  console.log(`Process incoming request: ${req.method} ${req.originalUrl}`);
   const token = req.headers.token;
   try {
-    jwt.verify(token, SECRET);
+    const data = jwt.verify(token, SECRET);
+    console.log(`Token successfully parsed: ${JSON.stringify(data)}`);
+    next();
   } catch(e) {
     const error = new Error('Wrong access token');
     error.code = 401;
@@ -23,19 +32,21 @@ app.use('/', (req, res, next) => {
   }
 });
 
-app.get('/led', async (req, res) => {
+app.get('/led', (req, res) => {
   const ledSettings = store.getData();
   return res.json(ledSettings);
 });
 
 app.post('/led/enable', async (req, res) => {
   await store.setData({ isEnabled: true });
+  console.log('Successfully enabled led.')
   res.status(200);
   res.end();
 });
 
 app.post('/led/disable', async (req, res) => {
   await store.setData({ isEnabled: false });
+  console.log('Successfully disabled led.')
   res.status(200);
   res.end();
 });
@@ -43,8 +54,15 @@ app.post('/led/disable', async (req, res) => {
 app.post('/led/color', async (req, res) => {
   const color = req.body.color;
   await store.setData({ color });
+  console.log('Successfully set led color.')
   res.status(200);
   res.end();
+});
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.code = 404;
+  next(error);
 });
 
 app.use((error, req, res, next) => {
